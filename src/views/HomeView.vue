ca4bd8cdb67f8b33fe1859c986108a4d68aa05de
Carlos Castro <carliianoide@hotmail.com>
2023-06-18 10:39:41 +0900

    feat: added stars effect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,12 @@
   <div
     class="flex flex-column align-items-center justify-content-center h-full sw-background"
   >
+    <div class="bg-animation">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      <div id="stars4"></div>
+    </div>
     <div class="sw-text mb-3 sw-title">STAR WARS SEARCH ENGINE</div>
     <star-wars-search />
   </div>
